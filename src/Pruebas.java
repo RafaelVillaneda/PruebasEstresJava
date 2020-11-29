da41f31299047b46009644748f400610d75125a3
Rafael Eulalio Villaneda de la Torre <rafaeua2401@outlook.com>
2020-11-28 18:11:55 -0600

    Se agrego el metodo de ordenamientoRadix

--- src/Pruebas.java
+++ src/Pruebas.java
@@ -157,7 +157,43 @@ class MetodosOrdenamientos{
         return numeros;
 }
 
-	
+	public void ordenamientoRadix(int[]numeros) {
+		contador[2]++;
+	      if(numeros.length == 0)
+	          return;
+	          int[][] np = new int[numeros.length][2];
+	          int[] q = new int[0x100];
+	          int i,j,k,l,f = 0;
+	          for(k=0;k<4;k++) {
+	             for(i=0;i<(np.length-1);i++)
+	             np[i][1] = i+1;
+	             np[i][1] = -1;
+	             for(i=0;i<q.length;i++)
+	             q[i] = -1;
+	             for(f=i=0;i<numeros.length;i++) {
+	                j = ((0xFF<<(k<<3))&numeros[i])>>(k<<3);
+	                contador[2]++;
+	                if(q[j] == -1)
+	                l = q[j] = f;
+	             else {
+	                l = q[j];
+	                while(np[l][1] != -1)
+	                l = np[l][1];
+	                np[l][1] = f;
+	                l = np[l][1];
+	                contador[0]++;
+	             }
+	             f = np[f][1];
+	             np[l][0] = numeros[i];
+	             np[l][1] = -1;
+	             contador[0]++;
+	          }
+	          for(l=q[i=j=0];i<0x100;i++)
+	          for(l=q[i];l!=-1;l=np[l][1])
+	        	  numeros[j++] = np[l][0];
+	          contador[0]++;
+	       }//for
+	}//metodo
 
 }
 public class Pruebas {
